7273af1c100d16a306503d1a710b40c6ae597142
xuser <xuser@localhost.localdomain>
2018-04-02 11:52:57 +0800

    merubah Makefile

--- testing/test.c
+++ testing/test.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#define TRUE 1
 
 int main(void){
 
-	printf("testing\n");
+	int num = 0;
+	char input;
+
+	while(TRUE){
+		printf("%d - ",num);
+		printf("Ulang lagi ? ");
+		scanf("\n%s",&input);
+
+		if((input == 'n') || (input == 'N')) break;
+		else{ num++; continue; }
+	}
+
+	printf("program selesai\n");
 
 	return 0;
 }
